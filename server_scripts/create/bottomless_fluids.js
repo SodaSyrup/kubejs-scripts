ec76a8b06e72e082817f541a32952b43f3878731
Salad <salad@bysalad.studio>
2025-12-05 19:14:22 +0100

    Delete bottomless_fluids.js

--- server_scripts/create/bottomless_fluids.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.tags('fluid', event => {
-    const fluids = [
-      'spelunkery:portal_fluid'
-    ];
-
-    event.add('create:bottomless/allow', fluids);
-
-    console.info('KubeJS: Finished bottomless fluids tag processing.');
-});
